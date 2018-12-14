c713994de8e8276fbcf1765edced26ecc2461d8c
Paulo Almeida <developer-ms@hotmail.com>
2018-12-14 20:21:12 -0200

    Começando a implementar o método que imprime o tabuleiro.

--- XadrezConsole/XadrezConsole/tabuleiro/Tabuleiro.cs
+++ XadrezConsole/XadrezConsole/tabuleiro/Tabuleiro.cs
@@ -13,5 +13,9 @@
             pecas = new Peca[linhas, colunas];
         }
 
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
+        }
     }
 }
