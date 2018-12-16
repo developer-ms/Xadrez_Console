6167e0ce1be40798273a3268a18977777fc38773
Paulo Almeida <developer-ms@hotmail.com>
2018-12-16 21:08:53 -0200

    Jogada especia promocao

--- XadrezConsole/XadrezConsole/xadrez/PartidaDeXadrez.cs
+++ XadrezConsole/XadrezConsole/xadrez/PartidaDeXadrez.cs
@@ -137,6 +137,20 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+            Peca p = tab.peca(destino);
+
+            // #jogada especia promocao
+            if (p is Peao)
+            {
+                if (p.cor == Cor.Branco && destino.linha == 0 || p.cor == Cor.Preta && destino.linha == 7)
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
 
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
@@ -155,7 +169,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-            Peca p = tab.peca(destino);
 
             // #jogada especial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
